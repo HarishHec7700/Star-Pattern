31433fc3c97c4150bb4c6f1a0b2986a7130360fa
HarishHec7700 <harish.hariharan7@gmail.com>
2022-11-03 21:41:00 +0530

    Hollow Up Right Triangle

---
+++ Hollow up right Triangle/script.js
@@ -0,0 +1,25 @@
+// To print the hollow right angle upper triangle in star
+
+
+let spc="",no;
+no=parseInt(prompt("Enter the number of times "));
+for(i=no;i>=1;i--){
+	for(j=1;j<=i;j++){
+		if(i==1||i==no){
+			spc+="* ";
+			//console.log(spc);
+		}
+		else{
+			if(j==1||j==i){
+				spc+="* ";
+			//console.log(spc);
+		}
+			 else{
+				spc+="  ";
+			}
+		}
+	}
+	spc+="\n"
+}
+
+console.log(spc);
