aaf38874e8e7fa6349c1625eb255266b45f69121
HarishHec7700 <harish.hariharan7@gmail.com>
2022-11-15 23:49:35 +0530

    Down Right Angle Triangle

---
+++ Down-right-angle-triangle/script.js
@@ -0,0 +1,9 @@
+// Down Right Angle Triangle
+
+ let num=parseInt(prompt("Enter the terms"));
+    for (i=0;i<num;i++){
+        for(j=0;j<=i;j++){
+            document.write("*");
+        }
+	document.write("<br>");	
+    }
